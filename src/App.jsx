1df36788c887c2ace00e1046e416a683541ee459
SamueleTerraroli <terraroli99@gmail.com>
2025-01-20 14:23:12 +0100

    feat: add Form

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Form from './components/Form'
+
+const App = () => {
+
+  return (
+    <div className='container mt-5'>
+      <h1>Blog</h1>
+      <Form />
+    </div>
+  )
+}
+
+export default App
